e3eb08e9eb0ec1e324e6e4ef7379777a6005aa61
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2023-05-22 20:48:05 +0200

    Update and rename administration.py to it.py

---
+++ Store/it.py
@@ -0,0 +1,38 @@
+# IN PROGRESS
+
+import accounts
+
+options = ['1. All accounts', '2. Add an account',
+           '3. Delete an account', '4. Logout']
+
+
+def menu_options():
+    print("-" * 35)
+    print('\t\tIT options:')
+    print("-" * 35)
+    for option in options:
+        print(option)
+    print()
+
+
+def user_choice():
+    menu_options()
+    should_continue = True
+    while should_continue:
+        try:
+            user_input = input('Choose an option: ')
+            if user_input == '1':
+                accounts.all_accounts()
+                menu_options()
+            elif user_input == '2':
+                accounts.add_an_account()
+                menu_options()
+            elif user_input == '3':
+                accounts.delete_an_account()
+                menu_options()
+            elif user_input == '4':
+                print('See you next time.')
+                should_continue = False
+        except ValueError:
+            print('Wrong input. Try again.')
+
