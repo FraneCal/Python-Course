c5d70186c5a8395419d284c88b588a90c84ee46c
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2023-05-22 20:49:06 +0200

    Update main.py

--- Store/main.py
+++ Store/main.py
@@ -9,25 +9,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# PRODAJA - IZLAZ
-# Logiranje s korisnickim imenom-lozinkom (izlaz-izlaz)
-# Meni: Stanje, Stanje po proizvodu (upit), Novi racun, Logout
-
-# Racun je dio liste svih racuna (mozemo vise racuna jedan za drugim napraviti)
-# Dodavanje proizvoda id ili opis, kolicina
-# Ispis u obliku tablice: Stavka (1,2,3), Opis proizvoda,
-# Kolicina, Jed.cijena, Ukupno za stavku, Ukupno racun
-# Izostaviti detalje poput PDV-a
-# Ispis računa SMANJUJE stanje proizvoda u listi proizvoda za odgovarajuću količinu!
-
-# ADMINISTRACIJA
-# Logiranje s korisnickim imenom-lozinkom(admin-admin)
-# Uvid u stanje na skladistu
-# Uvid u broj izdanih računa
-# Uvid u promet (u Eurima)
-# Jos ideja???
-
-# KUPAC
-# poslovni kupci, OIB, R1, itd.
