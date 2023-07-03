dba62096f96372cccaad8a2f2f72a735ea08bff6
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2023-07-03 18:50:10 +0200

    Update main.py

--- SQLite/main.py
+++ SQLite/main.py
@@ -1,4 +1,7 @@
-from database_class import Database
+#from database_class import Database
+
+# Uncomment the upper code and comment this one bellow to use the old version; else leave the code as it is :)
+from database_class_updated import Database
 
 database = Database()
 database.menu()
